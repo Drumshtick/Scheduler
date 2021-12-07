0c4111b69b9a3b94ace12cca5def830e0c6afbc3
Drumshtick <ekodnar@outlook.com>
2021-12-07 14:11:14 -0600

    added tests, passed tests

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,6 +4,19 @@ import styles from "./DayListItem.scss"
 
 
 export default function DayListItem(props) {
+
+  const formatSpots = (spots) => {
+    if (spots === 0) {
+      return "no spots remaining";
+    }
+
+    if (spots === 1) {
+      return "1 spot remaining";
+    }
+
+    return `${spots} spots remaining`
+  };
+
   const classes = classNames({
     'day-list__item': true,
     'day-list__item--selected': props.selected,
@@ -14,7 +27,7 @@ export default function DayListItem(props) {
   return (
     <li onClick={() => setDay(props.name)} className={classes}>
       <h2 className="text--regular">{name}</h2>
-      <h3 className="text--light">{spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(spots)}</h3>
     </li>
   );
 };
