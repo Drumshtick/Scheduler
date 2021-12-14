b3a8965f2c264c0a1ef5c36f7202d76a88545315
Drumshtick <ekodnar@outlook.com>
2021-12-14 15:25:55 -0600

    added feature - validate student name is not blank - added test for feature

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -8,6 +8,7 @@ export default function Form(props) {
 
   const [ student, setStudent ] = useState(props.student || "");
   const [ interviewer, setInterviewer ] = useState(props.interviewer || null);
+  const [ error, setError ] = useState("");
 
   const reset = () => {
     setStudent('');
@@ -18,19 +19,30 @@ export default function Form(props) {
     reset();
     onCancel();
   };
+
+  function validate() {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+
+    onSave(student, interviewer);
+  };
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
         <form autoComplete="off" onSubmit={(e) => e.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
-            name="name"
             type="text"
             placeholder="Enter Student Name"
             value={student}
             onChange={(e) => setStudent(e.target.value)}
+            data-testid="student-name-input"
           />
         </form>
+        <section className="appointment__validation" data-testid="error-msg">{error}</section>
         <InterviewerList 
           interviewers={interviewers}
           onChange={setInterviewer}
@@ -40,7 +52,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={() => onSave(student, interviewer)}>Save</Button>
+          <Button confirm onClick={validate}>Save</Button>
         </section>
       </section>
     </main>
