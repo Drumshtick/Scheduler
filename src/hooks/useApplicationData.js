0698ecc3f8a7d3f94226279770df8f378325f9dd
Drumshtick <ekodnar@outlook.com>
2022-04-07 22:50:22 -0600

    added fix for heroku server still offline

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -85,6 +85,9 @@ export default function useApplicationData() {
       axios.get('/api/interviewers')
     ])
       .then((all) => {
+        if (all.length === 0) {
+          window.location.reload(true);
+        }
         setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
       })
     }, []);
